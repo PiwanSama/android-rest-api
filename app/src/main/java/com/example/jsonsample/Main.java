91eb57770e4edfb98bd7f32c4727ef9716dc10a6
PiwanSama <piwan.summerelee@gmail.com>
2019-05-07 12:38:17 +0300

    Handled exceptions, Added Images and Landscape View

--- app/src/main/java/com/example/jsonsample/Main.java
+++ app/src/main/java/com/example/jsonsample/Main.java
@@ -3,8 +3,15 @@ package com.example.jsonsample;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.PagerSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -13,6 +20,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -21,32 +29,43 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 public class Main extends AppCompatActivity{
 
     ArrayList <String> users;
+    ArrayList <String> emails;
+    ArrayList <String> images;
     String TAG = "Main Activity";
     UserRecyclerView adapter;
+    SnapHelper snap;
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        snap = new PagerSnapHelper();
         users = new ArrayList<>();
-        Button load = findViewById(R.id.loadbtn);
-        load.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Start Async method to retrieve data
-                new GetUsers().execute();
-            }
-        });
+        emails = new ArrayList<>();
+        images = new ArrayList<>();
         RecyclerView rv = findViewById(R.id.users_recycler_view);
-        rv.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new UserRecyclerView(this,users);
+        rv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+        adapter = new UserRecyclerView(this,users,emails, images);
         rv.setAdapter(adapter);
+        snap.attachToRecyclerView(rv);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.load_button:
+                new GetUsers().execute();
+                break;
+        }
+        return true;
     }
 
     private class GetUsers extends AsyncTask<Void, Void, String>{
@@ -61,25 +80,32 @@ public class Main extends AppCompatActivity{
         protected String doInBackground(Void... voids){
             HttpURLConnection connection = null;
             BufferedReader reader = null;
-            String jsonUrl = "https://jsonplaceholder.typicode.com/users";
+            String jsonUrl = "http://192.168.43.220/JSONSam/Users/read.php";
             try {
                 URL url = new URL(jsonUrl);
                 connection = (HttpURLConnection)url.openConnection();
                 connection.setRequestMethod("GET");
                 connection.connect();
                 Log.i(TAG, "Connection opened");
+                if (connection.getResponseCode()== HttpURLConnection.HTTP_OK){
+                    InputStream stream  = connection.getInputStream();
+                    reader = new BufferedReader(new InputStreamReader(stream));
 
-                InputStream stream  = connection.getInputStream();
-                reader = new BufferedReader(new InputStreamReader(stream));
+                    StringBuffer buffer = new StringBuffer();
 
-                StringBuffer buffer = new StringBuffer();
+                    String line = "";
 
-                String line = "";
-
-                while ((line = reader.readLine())!=null){
-                    buffer.append(line+"\n");
+                    while ((line = reader.readLine())!=null){
+                        buffer.append(line+"\n");
+                    }
+                    return buffer.toString();
                 }
-                return buffer.toString();
+                else{
+                    Log.i(TAG, "Error code:"+connection.getResponseCode());
+                }
+            }
+            catch (FileNotFoundException e){
+                e.printStackTrace();
             }
             catch (MalformedURLException e) {
                 e.printStackTrace();
@@ -99,17 +125,24 @@ public class Main extends AppCompatActivity{
 
         protected void onPostExecute(String result){
             try {
-                JSONArray ja = new JSONArray(result);
+                JSONObject obj = new JSONObject(result);
+                JSONArray ja = obj.getJSONArray("Users");
                 for (int i = 0; i<ja.length();i++){
                     int x = i+1;
-                    JSONObject jObject = (JSONObject) ja.get(i);
-                    String name = jObject.getString("name");
-                    String toadd = x+". "+name;
+                    JSONObject userJsonObj = (JSONObject) ja.get(i);
+                    String name = userJsonObj.getString("Name");
+                    String email = userJsonObj.getString("Email");
+                    String image = userJsonObj.getString("Image");
+                    String toadd = name;
                     users.add(toadd);
+                    emails.add(email);
+                    images.add(image);
                     adapter.notifyDataSetChanged();
-                    Log.i("Added:", name);
                 }
             } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            catch (NullPointerException e){
                 e.printStackTrace();
             }
             View parent = findViewById(android.R.id.content);
--- app/src/main/java/com/example/jsonsample/UserRecyclerView.java
+++ app/src/main/java/com/example/jsonsample/UserRecyclerView.java
@@ -1,35 +1,53 @@
 package com.example.jsonsample;
 
 import android.content.Context;
+import android.content.res.Configuration;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.github.abdularis.civ.CircleImageView;
+import com.squareup.picasso.Picasso;
+
 import java.util.List;
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
 
 public class UserRecyclerView extends RecyclerView.Adapter <UserRecyclerView.ViewHolder>{
 
-    private List<String> userss;
+    private List<String> users;
+    private List<String> emails;
+    private List<String> images;
+    Context context;
     private LayoutInflater mInflater;
 
     //Create Constructor and pass data to it
 
-    UserRecyclerView(Context context, List<String> data){
-        this.userss = data;
+    UserRecyclerView(Context context, List<String> data, List<String> mails, List<String> pics){
+        this.users = data;
+        this.emails = mails;
+        this.context=context;
+        this.images = pics;
         this.mInflater = LayoutInflater.from(context);
     }
 
 
 //Create ViewHolder Class and set onClickListener for the item view
     public class ViewHolder extends RecyclerView.ViewHolder{
-        TextView myNameView;
+        TextView myNameView, mMail;
+        CircleImageView img;
+        CardView card;
         public ViewHolder (View itemView){
             super(itemView);
+            card = itemView.findViewById(R.id.usercard);
             myNameView = itemView.findViewById(R.id.mname);
+            mMail = itemView.findViewById(R.id.memail);
+            img = itemView.findViewById(R.id.icon_img);
         }
     }
 
@@ -39,20 +57,38 @@ public class UserRecyclerView extends RecyclerView.Adapter <UserRecyclerView.Vie
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = mInflater.inflate(R.layout.user_row, parent, false);
-        return new ViewHolder(view);
+        View itemView = null;
+        int orientation = context.getResources().getConfiguration().orientation;
+        switch (orientation){
+            case Configuration.ORIENTATION_LANDSCAPE:
+                itemView = LayoutInflater.from(parent.getContext()).
+                        inflate((R.layout.user_row_land), parent, false);
+                //view = mInflater.inflate(R.layout.user_row_land, parent, false);
+                Log.i("Adapter", "Land");
+                break;
+            case Configuration.ORIENTATION_PORTRAIT:
+                itemView = LayoutInflater.from(parent.getContext()).
+                        inflate((R.layout.user_row), parent, false);
+                //view = mInflater.inflate(R.layout.user_row, parent, false);
+                Log.i("Adapter", "Portrait");
+                break;
+        }
+        return new ViewHolder(itemView);
     }
 
     //Bind one data item in the array list to an item in the row
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        String user = userss.get(position);
+        String user = users.get(position);
+        String email = emails.get(position);
+        Picasso.with(context).load("http://192.168.43.220/JSONSam/Images/"+images.get(position)).into(holder.img);
         holder.myNameView.setText(user);
+        holder.mMail.setText(email);
     }
 
     @Override
     public int getItemCount() {
-        return userss.size();
+        return users.size();
     }
 
 }
